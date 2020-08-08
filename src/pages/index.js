36aedbba0bd8cf21a524ad75f3ff40d3f61c5526
lulu70 <liorco70@yahoo.com>
2020-08-08 09:27:06 +0200

    initial commit from lior - added bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
-
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Container className="text-primary bg-dark">
+      <Row>
+        <Col xs={3}>Hello world!</Col>
+        <Col>Hello world!</Col>
+      </Row>
+    </Container>
+  )
 }
