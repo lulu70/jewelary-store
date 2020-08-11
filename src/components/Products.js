02c8870b352f5f9955c7eed58cd315f092b7b6f0
lulu70 <liorco70@yahoo.com>
2020-08-11 17:34:23 +0200

    changed styling

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,7 @@
 import React from "react"
-import Container from "react-bootstrap/Container"
+import Button from "react-bootstrap/Button"
 import Row from "react-bootstrap/Row"
+import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
 import GatsbyImage from "gatsby-image"
 import { graphql, useStaticQuery, Link } from "gatsby"
@@ -26,6 +27,7 @@ const Products = () => {
             priceRange {
               maxVariantPrice {
                 amount
+                currencyCode
               }
             }
             availableForSale
@@ -36,22 +38,59 @@ const Products = () => {
     `
   )
   return (
-    <Container>
+    <>
       <h2 className="text-center mb-5 display-4">Products</h2>
       <Row className="justify-content-md-between justify-content-center">
         {data.products.nodes.map(product => (
-          <Col xs={10} md={6} lg={4} key={product.id} className="mb-5">
-            <Link to={`/product/${product.handle}`}>
-              <GatsbyImage
-                fluid={product.images[0].localFile.childImageSharp.fluid}
-                alt={product.name}
-                className="border"
-              />
-            </Link>
-          </Col>
+          <Card
+            key={product.id}
+            bg="secondary"
+            className="mb-5 p-0"
+            as={Col}
+            xs={8}
+            md={5}
+            lg={3}
+          >
+            <Card.Img
+              as={GatsbyImage}
+              fluid={product.images[0].localFile.childImageSharp.fluid}
+              alt={product.name}
+              variant="top"
+            />
+            <Card.Body className="p-1">
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>
+                {product.priceRange.maxVariantPrice.amount}{" "}
+                {product.priceRange.maxVariantPrice.currencyCode}
+              </Card.Text>
+              <Button
+                as={Link}
+                variant="dark"
+                block
+                to={`/product/${product.handle}`}
+              >
+                View Product
+              </Button>
+            </Card.Body>
+          </Card>
+          // <Col xs={10} md={6} lg={4} key={product.id} className="mb-5">
+          //   <GatsbyImage
+          //     fluid={product.images[0].localFile.childImageSharp.fluid}
+          //     alt={product.name}
+          //   />
+          //   <Button
+          //     as={Link}
+          //     variant="outline-light"
+          //     block
+          //     to={`/product/${product.handle}`}
+          //     className="rounded-0"
+          //   >
+          //     View
+          //   </Button>
+          // </Col>
         ))}
       </Row>
-    </Container>
+    </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,34 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Jumbotron from "react-bootstrap/Jumbotron"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import SiteCarousel from "../components/SiteCarousel"
 import Products from "../components/Products"
 import SEO from "../components/SEO"
-
+import svg from "../assets/hands.svg"
 export default function Home() {
   return (
     <Layout>
       <SEO title="HOME" />
       <SiteCarousel />
-      <Jumbotron className="bg-dark text-light text-center col-md-6 mx-auto">
-        <h1 className="display-1">Hello, world!</h1>
-        <p className="lead">
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
-      </Jumbotron>
-      <Products />
+      <Container>
+        <Row className="align-items-center justify-content-center">
+          <Col xs={2} />
+          <Jumbotron className="bg-dark text-light text-center col-md-8">
+            <h1 className="display-3">Hello, world!</h1>
+            <p className="lead">
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+          </Jumbotron>
+          <Col xs={2} className="mb-4 d-none d-md-block">
+            <img src={svg} alt="hands" className="w-75" />
+          </Col>
+        </Row>
+        <Products />
+      </Container>
     </Layout>
   )
 }
