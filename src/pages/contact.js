0428814170386a31d06a067c67d14ebe78ddeae7
lulu70 <liorco70@yahoo.com>
2020-08-18 22:40:03 +0200

    added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const Contact = () => {
+  return (
+    <Layout>
+      <form name="contact" netlify>
+        <p>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </Layout>
+  )
+}
+
+export default Contact
