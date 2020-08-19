56cd1f684bc00ce92f903292061bd09d1f39fa42
lulu70 <liorco70@yahoo.com>
2020-08-19 07:26:05 +0200

    added a new contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,25 +1,79 @@
 import React from "react"
-import Layout from "../components/Layout"
+import { navigateTo } from "gatsby-link"
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
 
 const Contact = () => {
+  const [state, setState] = React.useState({})
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
   return (
-    <Layout>
-      <form action="POST" name="contact" data-netlify="true" netlify>
-        <p>
+    <div>
+      <h1>Contact</h1>
+      <form
+        name="contact"
+        method="post"
+        action="/thanks-for-contacting/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
           <label>
-            Name <input type="text" name="name" />
+            Don’t fill this out:{" "}
+            <input name="bot-field" onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Email <input type="email" name="email" />
+            Your name:
+            <br />
+            <input type="text" name="name" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
           </label>
         </p>
         <p>
           <button type="submit">Send</button>
         </p>
       </form>
-    </Layout>
+    </div>
   )
 }
 
---
+++ src/pages/thanks-for-contacting.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const thanksForContacting = () => {
+  return (
+    <Layout>
+      <h1>Thanks for contacting...</h1>
+    </Layout>
+  )
+}
+
+export default thanksForContacting
